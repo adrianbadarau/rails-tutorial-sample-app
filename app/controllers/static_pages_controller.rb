8a9c53f575143cc411a0cd6d08193815d1e0d08f
Adrian Badarau <adi.badarau@yahoo.com>
2014-08-29 00:14:43 +0300

    fixed the login bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micro_post = current_user.micro_posts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if signed_in?
+      @micro_post = current_user.micro_posts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
