0484ca1764de71bc129be16322b33cbeeec6ead3
Adrian Badarau <adi.badarau@yahoo.com>
2014-09-23 07:09:31 +0300

    Following Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,25 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 6}
 
   has_many :micro_posts, dependent: :destroy
+  has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   def feed
-    MicroPost.where("user_id = ?", id)
+    MicroPost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    self.relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    self.relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def User.new_remember_token
